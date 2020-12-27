83dc0d6c9585396599605e3f3be93de29d55ccf8
ZeroTimeX <76637252+ZeroTimeX@users.noreply.github.com>
2020-12-27 22:54:18 +0800

    Update textnow_sms.py

--- scripts/textnow/textnow_sms.py
+++ scripts/textnow/textnow_sms.py
@@ -101,9 +101,9 @@ class Textnow:
     driver.execute_script("if(!window.jQuery){var scriptEle=document.createElement('script');scriptEle.src='https://cdn.jsdelivr.net/gh/jquery/jquery@3.2.1/dist/jquery.min.js';document.body.append(scriptEle)}")
     time.sleep(3)
     
-    driver.execute_script("$('#recent-header .toast-container').remove();")
-    driver.execute_script("$('.notification-priming-modal').remove();")
-    driver.execute_script("$('.modal').remove();")
+    #driver.execute_script("$('#recent-header .toast-container').remove();")
+    #driver.execute_script("$('.notification-priming-modal').remove();")
+    #driver.execute_script("$('.modal').remove();")
     time.sleep(2)
     
     for phone in self.PHONE_NUMBER.split(','):
