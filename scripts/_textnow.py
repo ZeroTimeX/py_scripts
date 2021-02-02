9460c8bcfd2e2de52d6e742be9872d8f751a4c0d
ZeroTimeX <76637252+ZeroTimeX@users.noreply.github.com>
2021-02-02 19:55:54 +0800

    Update _textnow.py

--- scripts/_textnow.py
+++ scripts/_textnow.py
@@ -12,22 +12,23 @@ if len(usernames) != len(passwords):
   quit()
 else:
   print(u"共有 %s 个账号，即将开始保号处理" % len(usernames))
-
-numbers = '(505) 302-1529'
-msg = 'code:'+str(random.randint(100000,999999))
-for idx in range(0,len(usernames)):
-  username=usernames[idx]
-  password=passwords[idx]
-  text = textnow_sms.Textnow(username, password, numbers, msg)
-  text.send_text()
-  print("---第%s个账号处理完毕---" % (idx+1))
-
-numbers = '(970) 638-3082'
-msg = 'code:'+str(random.randint(100000,999999))
-for idx in range(0,len(usernames)):
-  username=usernames[idx]
-  password=passwords[idx]
-  text = textnow_sms.Textnow(username, password, numbers, msg)
-  text.send_text()
-  print("---第%s个账号处理完毕---" % (idx+3))
+x=random.randint(1,100)
+if x<50
+  numbers = '(505) 302-1529'
+  msg = 'code:'+str(random.randint(100000,999999))
+  for idx in range(0,len(usernames)):
+    username=usernames[idx]
+    password=passwords[idx]
+    text = textnow_sms.Textnow(username, password, numbers, msg)
+    text.send_text()
+    print("---第%s个账号处理完毕---" % (idx+1))
+if x>50
+  numbers = '(970) 638-3082'
+  msg = 'code:'+str(random.randint(100000,999999))
+  for idx in range(0,len(usernames)):
+    username=usernames[idx]
+    password=passwords[idx]
+    text = textnow_sms.Textnow(username, password, numbers, msg)
+    text.send_text()
+    print("---第%s个账号处理完毕---" % (idx+3))
 print("---Good Job! 所有账号处理完毕---")
