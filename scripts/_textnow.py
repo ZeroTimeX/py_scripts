42fcaf74f8177a7120f28b4e403b4f1d0b81728b
ZeroTimeX <76637252+ZeroTimeX@users.noreply.github.com>
2021-02-14 20:28:15 +0800

    Update _textnow.py

--- scripts/_textnow.py
+++ scripts/_textnow.py
@@ -4,31 +4,27 @@ from textnow import textnow_sms
 import os
 import random
 
-usernames = os.environ["TEXTNOW_USERNAME"].split(',')
-passwords = os.environ["TEXTNOW_PASSWORD"].split(',')
+tn = os.environ["TN"]
+tn2 = os.environ["TN2"]
 
-if len(usernames) != len(passwords):
-  print(u"账号和密码个数不对应")
-  quit()
-else:
-  print(u"共有 %s 个账号，即将开始保号处理" % len(usernames))
+print(u"即将开始保号处理")
 x=random.randint(1,100)
 if x<50:
   numbers = '5053021529'
   msg = 'code:'+str(random.randint(100000,999999))
-  for idx in range(0,len(usernames)):
-    username=usernames[idx]
-    password=passwords[idx]
-    text = textnow_sms.Textnow(username, password, numbers, msg)
-    text.send_text()
-    print("---第%s个账号处理完毕---" % (idx+1))
-if x>50:
+  text = textnow_sms.Textnow(tn, numbers, msg)
+  text.send_text()
+  print("---第1个账号处理完毕---")
+  text = textnow_sms.Textnow(tn2, numbers, msg)
+  text.send_text()
+  print("---第2个账号处理完毕---")
+if x>=50:
   numbers = '9706383082'
   msg = 'code:'+str(random.randint(100000,999999))
-  for idx in range(0,len(usernames)):
-    username=usernames[idx]
-    password=passwords[idx]
-    text = textnow_sms.Textnow(username, password, numbers, msg)
-    text.send_text()
-    print("---第%s个账号处理完毕---" % (idx+3))
+  text = textnow_sms.Textnow(tn, numbers, msg)
+  text.send_text()
+  print("---第1个账号处理完毕---")
+  text = textnow_sms.Textnow(tn2, numbers, msg)
+  text.send_text()
+  print("---第2个账号处理完毕---")
 print("---Good Job! 所有账号处理完毕---")
